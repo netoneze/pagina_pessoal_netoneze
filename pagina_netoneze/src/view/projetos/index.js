b90a95e8a76bade8c7c1169a4a8da47a5b26f470
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-31 13:57:40 -0300

    Corrigido texto e classes

--- pagina_netoneze/src/view/projetos/index.js
+++ pagina_netoneze/src/view/projetos/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import Proj1 from '../../img/proj1.png'
 import Proj2 from '../../img/proj2.png'
@@ -11,11 +11,12 @@ function Projetos() {
     return (
             <main className="item-container" id="itemPrincipal">
                 <div className="principal">
-                    <h2 className="titulo-principal">Projetos</h2>
-
+                    <div className="saudacoes-principal">
+                        <h4 className="titulo-principal">Projetos</h4>
+                    </div>
                     <h2 className="titulo-principal">Nobi's World Adventure</h2>
                     <a href="https://play.google.com/store/apps/details?id=com.netoneze.NobisWorldAdventure&hl=pt">
-                        <img className="imagens-corpo" src={Proj1} alt="projeto1"/>
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj1} alt="projeto1" title="Game 1"/>
                     </a>
                     <p>
                         Um jogo estilo <i>Runner</i> para <strong>Android</strong>, desenvolvido em C# com o auxílio da
@@ -24,9 +25,9 @@ function Projetos() {
 
                     <br/>
 
-                    <h2 className="titulo-principal">Super Adventure Kid</h2>
+                    <h2 className="titulo-principal titulo-projeto">Super Adventure Kid</h2>
                     <a href="https://play.google.com/store/apps/details?id=com.netoneze.SuperAdventureKidFree&hl=pt">
-                        <img className="imagens-corpo" src={Proj2} alt="projeto2"/>
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj2} alt="projeto2" title="Game 2"/>
                     </a>
                     <p>
                         Um jogo estilo <i>Runner</i> para <strong>Android</strong>, desenvolvido em C# com o auxílio
@@ -35,9 +36,9 @@ function Projetos() {
 
                     <br/>
 
-                    <h2 className="titulo-principal">NelsonGenda (Work in Progress)</h2>
+                    <h2 className="titulo-principal titulo-projeto titulo-projeto-proj3">NelsonGenda (Work in Progress)</h2>
                     <a href="https://play.google.com/store/apps/details?id=com.nelsongenda.agenda&hl=pt">
-                        <img className="imagens-corpo" src={Proj3} alt="projeto3"/>
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj3} alt="projeto3" title="NelsonGenda"/>
                     </a>
                     <p>
                         Uma <strong>agenda</strong> simples e fácil de ser usada. Desenvolvido em Java com o
@@ -46,9 +47,9 @@ function Projetos() {
 
                     <br/>
 
-                    <h2 className="titulo-principal">JPedidos</h2>
+                    <h2 className="titulo-principal titulo-projeto">JPedidos</h2>
                     <a href="https://github.com/netoneze/JPedidos">
-                        <img className="imagens-corpo" src={Proj4} alt="projeto4"/>
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj4} alt="projeto4" title="Jpedidos"/>
                     </a>
                     <p>
                         Um <strong>sistema de pedidos</strong> feito em Java, com base de dados em MySQL.
