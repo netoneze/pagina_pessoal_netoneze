0181b68b3c70a8e6baee3069996b440e311154c0
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-09-21 16:42:46 -0300

    Reorganizado ordem dos projetos

--- pagina_netoneze/src/view/projetos/index.js
+++ pagina_netoneze/src/view/projetos/index.js
@@ -14,29 +14,18 @@ function Projetos() {
                     <div className="saudacoes-principal">
                         <h4 className="titulo-principal">Projetos</h4>
                     </div>
-                    <h2 className="titulo-principal">Nobi's World Adventure</h2>
-                    <a href="https://play.google.com/store/apps/details?id=com.netoneze.NobisWorldAdventure&hl=pt">
-                        <img className="imagens-corpo imagens-corpo-proj" src={Proj1} alt="projeto1" title="Game 1"/>
+
+                    <h2 className="titulo-principal titulo-projeto">Easy Task Manager</h2>
+                    <a href="https://play.google.com/store/apps/details?id=com.netoneze.easytaskmanager&hl=pt">
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj2} alt="projeto2" title="Easy Task Manager"/>
                     </a>
                     <p>
-                        Um jogo estilo <i>Runner</i> para <strong>Android</strong>, desenvolvido em C# com o auxílio da
-                        ferramenta <strong>Unity</strong>.
+                        Um aplicativo de <strong>gerenciamento de tarefas</strong> desenvolvido em Java, utilizando padrões ROOM e MVC.
                     </p>
 
                     <br/>
 
-                    <h2 className="titulo-principal titulo-projeto">Super Adventure Kid</h2>
-                    <a href="https://play.google.com/store/apps/details?id=com.netoneze.SuperAdventureKidFree&hl=pt">
-                        <img className="imagens-corpo imagens-corpo-proj" src={Proj2} alt="projeto2" title="Game 2"/>
-                    </a>
-                    <p>
-                        Um jogo estilo <i>Runner</i> para <strong>Android</strong>, desenvolvido em C# com o auxílio
-                        da ferramenta <strong>Unity</strong>.
-                    </p>
-
-                    <br/>
-
-                    <h2 className="titulo-principal titulo-projeto titulo-projeto-proj3">NelsonGenda (Work in Progress)</h2>
+                    <h2 className="titulo-principal titulo-projeto titulo-projeto-proj3">NelsonGenda</h2>
                     <a href="https://play.google.com/store/apps/details?id=com.nelsongenda.agenda&hl=pt">
                         <img className="imagens-corpo imagens-corpo-proj" src={Proj3} alt="projeto3" title="NelsonGenda"/>
                     </a>
@@ -53,6 +42,17 @@ function Projetos() {
                     </a>
                     <p>
                         Um <strong>sistema de pedidos</strong> feito em Java, com base de dados em MySQL.
+                    </p>
+
+                    <br/>
+
+                    <h2 className="titulo-principal">Nobi's World Adventure</h2>
+                    <a href="https://play.google.com/store/apps/details?id=com.netoneze.NobisWorldAdventure&hl=pt">
+                        <img className="imagens-corpo imagens-corpo-proj" src={Proj1} alt="projeto1" title="Game 1"/>
+                    </a>
+                    <p>
+                        Um jogo estilo <i>Runner</i> para <strong>Android</strong>, desenvolvido em C# com o auxílio da
+                        ferramenta <strong>Unity</strong>.
                     </p>
                 </div>
             </main>
